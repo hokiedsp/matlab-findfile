2a7c40b53e81f0eaaf550ba0dd2ad45d6d01c115
Melda Kunduk <Melda Kunduk@HS-COMD-58a.lsu.edu>
2018-03-28 21:31:29 -0500

    added 2 more arguments

--- findfile.cpp
+++ findfile.cpp
@@ -8,6 +8,7 @@
 static FINDEX_SEARCH_OPS search_ops(FindExSearchNameMatch);
 static DWORD flags(0);
 static std::wstring hint;
+static int maxhits(0);
 static std::deque<mxArray *> hits;
 
 void file_search(const std::wstring &path)
@@ -16,7 +17,7 @@ void file_search(const std::wstring &path)
     size_t len = in.size();
     mwSize dims[2]{1, len};
     mxArray *out = mxCreateCharArray(2, dims);
-    std::copy_n(in.c_str(), len, (wchar_t*)mxGetData(out));
+    std::copy_n(in.c_str(), len, (wchar_t *)mxGetData(out));
     return out;
   };
 
@@ -29,6 +30,8 @@ void file_search(const std::wstring &path)
     do
     {
       hits.push_back(utf16ToMxArray(path + FindFileData.cFileName));
+      if (maxhits > 0 && hits.size() > maxhits)
+        break;
     } while (FindNextFileW(hFind, &FindFileData));
   }
   FindClose(hFind);
@@ -38,6 +41,8 @@ void traverse_dirs(std::wstring directory)
 {
   // run the file search on the current directory
   file_search(directory);
+  if (maxhits > 0 && hits.size() > maxhits)
+    return;
 
   // recurse on its subdirectories
   std::wstring tmp = directory + L"*";
@@ -53,7 +58,7 @@ void traverse_dirs(std::wstring directory)
           continue;
       }
 
-      tmp = directory + std::wstring((wchar_t*)file.cFileName);
+      tmp = directory + std::wstring((wchar_t *)file.cFileName);
 
       if (file.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
         traverse_dirs(tmp + L"\\");
@@ -65,8 +70,10 @@ void traverse_dirs(std::wstring directory)
 
 // filepaths = findfile('hint',K,...)
 // Options:
-// - 'dir'
+// - 'dironly'
 // - 'recursive'
+// - 'basedir' 'path'
+// - 'casesensitive'
 void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 {
   if (!nrhs)
@@ -74,18 +81,56 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 
   if (!mxIsChar(prhs[0]))
     mexErrMsgIdAndTxt("findfile:invalidArguments", "First input argument must be a string");
-
   size_t hint_len = mxGetNumberOfElements(prhs[0]);
   wchar_t *hint_src = (wchar_t *)mxGetChars(prhs[0]);
+
+  if (nrhs >1 )
+  {
+    if (!(mxIsNumeric(prhs[1]) && mxGetNumberOfElements(prhs[1])<=1))
+      mexErrMsgIdAndTxt("findfile:invalidArguments", "Second input argument must be a finite integer scalar.");
+    if (mxIsEmpty(prhs[1]))
+      maxhits = 0;
+    else
+    {
+      double val = mxGetScalar(prhs[1]);
+      if (val<0 || !mxIsFinite(val) || val!=(double)(int)val)
+        mexErrMsgIdAndTxt("findfile:invalidArguments", "Second input argument must be a finite integer scalar.");
+      maxhits = (int)val;
+    }
+  }
+  else
+    maxhits = 0;
+
+  std::wstring basedir;
+  if (nrhs > 2)
+  {
+    if (!mxIsChar(prhs[2]))
+      mexErrMsgIdAndTxt("findfile:invalidArguments", "Third input argument must be a string");
+    basedir.assign((wchar_t *)mxGetChars(prhs[2]));
+  }
+
   hint.assign(hint_src, hint_src + hint_len); // global
 
   // traverse subdirectories
-  if ((hint[1]==':') || (hint[0]=='.') || (hint[0]=='\\')) // full path given
-    traverse_dirs(L"");
+  if ((hint[1] == ':') || (hint[0] == '.') || (hint[0] == '\\')) // full path given
+  {
+    if (basedir.size())
+      mexErrMsgIdAndTxt("findfile:mixedCommands", "Cannot specify base directory to search with a full path.");
+    file_search(hint);
+  }
   else
-    traverse_dirs(L".\\");
-  
+  {
+    if (basedir.empty())
+      basedir.assign(L".\\");
+    else if (basedir.back() != '\\')
+      basedir.append(L"\\");
+    traverse_dirs(basedir);
+  }
+
   plhs[0] = mxCreateCellMatrix(hits.size(), 1);
   for (int i = 0; i < hits.size(); ++i)
-    mxSetCell(plhs[0], i, hits[i]);
+  {
+    mxSetCell(plhs[0], i, hits.front());
+    hits.pop_front();
+  }
 }
